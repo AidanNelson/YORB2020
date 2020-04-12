4e2d436056af3573a478f7c33e89a04f2712c803
AidanNelson <AidanNelson@users.noreply.github.com>
2020-04-12 14:43:38 -0400

    broke floor model into separate .glb files w/o materials, added materials in three.js, cleaned up code in scene.js, adjusted lighting, moved hyperlink, removed scene.crazycolors() function

--- public/js/scene.js
+++ public/js/scene.js
@@ -1,9 +1,5 @@
 /* 
 * YORB 2020
-* 
-* Adapted from a THREE.js Multiplayer boilerplate made by Or Fleisher:
-* https://github.com/juniorxsound/THREE.Multiplayer
-* with terrain from https://github.com/mrdoob/three.js/blob/master/examples/webgl_geometry_terrain.html
 *
 * Aidan Nelson, April 2020
 *
@@ -21,19 +17,265 @@ class Scene {
 
 		//THREE scene
 		this.scene = new THREE.Scene();
-		this.framecount = 0;
 		this.keyState = {};
 
 		//Utility
 		this.width = _width;
 		this.height = _height;
+		this.stats = new Stats();
+		domElement.appendChild(this.stats.dom);
 
 		// crazy colors:
 		this.crazyMode = false;
 		this.originalFloorMaterials = [];
 
-
 		//Add Player
+		this.addSelf();
+
+		// Raycaster
+		this.raycaster = new THREE.Raycaster();
+
+		// add lights
+		this.addLights();
+
+		//THREE Camera
+		this.camera = new THREE.PerspectiveCamera(60, this.width / this.height, 0.1, 5000);
+		this.camera.position.set(0, 3, 3);
+		this.scene.add(this.camera);
+
+		// create an AudioListener and add it to the camera
+		this.listener = new THREE.AudioListener();
+		// this.camera.add(this.listener);
+		this.playerGroup.add(this.listener);
+
+		//THREE WebGL renderer
+		this.renderer = new THREE.WebGLRenderer({
+			antialiasing: true
+		});
+		this.renderer.shadowMap.enabled = true;
+		this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+		this.renderer.setClearColor(new THREE.Color(clearColor));
+		this.renderer.setSize(this.width, this.height);
+
+		// add controls:
+		// https://github.com/PiusNyakoojo/PlayerControls
+		this.controls = new THREE.PlayerControls(this.camera, this.playerGroup);
+
+		// array to store interactable hyperlinked meshes
+		this.hyperlinkedObjects = [];
+		// proof of concept hyperlink
+		this.createHyperlinkedMesh(-25, 0, -15, "http://www.example.com");
+
+		// environment map from three.js examples
+		this.loadBackground();
+
+		// load floor model
+		this.createMaterials();
+		this.loadFloorModel();
+
+		//Push the canvas to the DOM
+		domElement.append(this.renderer.domElement);
+
+		//Setup event listeners for events and handle the states
+		window.addEventListener('resize', e => this.onWindowResize(e), false);
+		domElement.addEventListener('mouseenter', e => this.onEnterCanvas(e), false);
+		domElement.addEventListener('mouseleave', e => this.onLeaveCanvas(e), false);
+		window.addEventListener('keydown', e => this.onKeyDown(e), false);
+		window.addEventListener('keyup', e => this.onKeyUp(e), false);
+
+		this.helperGrid = new THREE.GridHelper(500, 500);
+		this.helperGrid.position.y = -0.1; // offset the grid down to avoid z fighting with floor
+		this.scene.add(this.helperGrid);
+
+		this.scene.add(new THREE.AxesHelper(10));
+
+		this.update();
+	}
+
+
+	//////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////
+	// Lighting 💡
+
+	addLights() {
+		/ add some lights/
+		this.scene.add(new THREE.AmbientLight(0xffffe6, 0.7));
+
+		//https://github.com/mrdoob/three.js/blob/master/examples/webgl_lights_hemisphere.html
+		// main sunlight with shadows
+		let dirLight = new THREE.DirectionalLight(0xffffe6, 0.7);
+		dirLight.color.setHSL(0.1, 1, 0.95);
+		dirLight.position.set(- 1, 0.5, -1);
+		dirLight.position.multiplyScalar(200);
+		this.scene.add(dirLight);
+
+		dirLight.castShadow = true;
+		dirLight.shadow.mapSize.width = 1024;
+		dirLight.shadow.mapSize.height = 1024;
+
+		var d = 150;
+		dirLight.shadow.camera.left = - d;
+		dirLight.shadow.camera.right = d;
+		dirLight.shadow.camera.top = d;
+		dirLight.shadow.camera.bottom = - d;
+
+		dirLight.shadow.camera.far = 3500;
+		dirLight.shadow.bias = - 0.0001;
+
+		let dirLightHeper = new THREE.DirectionalLightHelper(dirLight, 10);
+		this.scene.add(dirLightHeper);
+
+		// secondary directional light without shadows:
+		let dirLight2 = new THREE.DirectionalLight(0xffffff, 0.5);
+		dirLight2.color.setHSL(0.1, 1, 0.95);
+		dirLight2.position.set(1, 0.5, -1);
+		dirLight2.position.multiplyScalar(200);
+		this.scene.add(dirLight2);
+	}
+
+	//////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////
+	// Model 🏗
+
+	loadBackground() {
+		var path = 'models/Park2/';
+		var format = '.jpg';
+		this.envMap = new THREE.CubeTextureLoader().load([
+			path + 'posx' + format, path + 'negx' + format,
+			path + 'posy' + format, path + 'negy' + format,
+			path + 'posz' + format, path + 'negz' + format
+		]);
+		this.scene.background = this.envMap;
+	}
+
+	// this method instantiates materials for various parts of the ITP floor model
+	// wall, ceiling, floor
+	createMaterials() {
+		this.testMaterial = new THREE.MeshLambertMaterial({ color: 0xffff1a });
+
+		let paintedRoughnessTexture = new THREE.TextureLoader().load("textures/roughness.jpg");
+		paintedRoughnessTexture.wrapS = THREE.RepeatWrapping;
+		paintedRoughnessTexture.wrapT = THREE.RepeatWrapping;
+		paintedRoughnessTexture.repeat.set(5, 5);
+
+		// wall material:
+		this.wallMaterial = new THREE.MeshPhongMaterial({
+			color: 0xfffff5,
+			bumpMap: paintedRoughnessTexture,
+			bumpScale: 0.25,
+			specular: 0xfffff5,
+			reflectivity: 0.01,
+			shininess: 0.1,
+			envMap: null
+		});
+
+		// ceiling material
+		this.ceilingMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff });
+
+		// floor material
+		// https://github.com/mrdoob/three.js/blob/master/examples/webgl_materials_variations_phong.html
+		let floorTexture = new THREE.TextureLoader().load("textures/floor.jpg");
+		floorTexture.wrapS = THREE.RepeatWrapping;
+		floorTexture.wrapT = THREE.RepeatWrapping;
+		floorTexture.repeat.set(1, 1);
+
+		this.floorMaterial = new THREE.MeshPhongMaterial({
+			color: 0xffffff,
+			map: floorTexture,
+			bumpMap: floorTexture,
+			bumpScale: 0.005,
+			specular: 0xffffff,
+			reflectivity: 0.5,
+			shininess: 4,
+			envMap: null
+		});
+
+		this.paintedMetalMaterial = new THREE.MeshPhongMaterial({
+			color: 0x1a1a1a,
+			bumpMap: paintedRoughnessTexture,
+			bumpScale: 0.2,
+			specular: 0xffffff,
+			reflectivity: 0.01,
+			shininess: 1,
+			envMap: null
+		});
+
+		this.windowShelfMaterial = new THREE.MeshPhongMaterial({
+			color: 0xdddddd
+		});
+
+		// https://github.com/mrdoob/three.js/blob/master/examples/webgl_materials_physical_transparency.html
+		this.glassMaterial = new THREE.MeshPhysicalMaterial({
+			color: 0xD9ECFF,
+			metalness: 0.05,
+			roughness: 0,
+			alphaTest: 0.5,
+			depthWrite: false,
+			envMap: this.envMap,
+			envMapIntensity: 1,
+			transparency: 1, // use material.transparency for glass materials
+			opacity: 1,                        // set material.opacity to 1 when material.transparency is non-zero
+			transparent: true
+		});
+
+		this.lightHousingMaterial = new THREE.MeshPhongMaterial({ color: 0x111111 });
+
+		this.lightDiffuserMaterial = new THREE.MeshPhongMaterial({
+			color: 0xcccccc,
+			emissive: 0xffffff,
+			emissiveIntensity: 10,
+			specular: 0xffffff,
+			reflectivity: 0.01,
+			shininess: 1,
+			envMap: null
+		});
+
+		this.glassFixturingMaterial = new THREE.MeshPhongMaterial({ color: 0x000000 });
+
+		this.graniteBarMaterial = new THREE.MeshPhongMaterial({ color: 0x000000 });
+	}
+
+	loadModel(_file, _material, _scale, _castShadow, _receiveShadow) {
+		this.GLTFLoader.load(_file, (gltf) => {
+			let scene = gltf.scene;
+			scene.position.set(0, 0, 0);
+			scene.scale.set(_scale, _scale, _scale);
+			scene.traverse((child) => {
+				if (child.isMesh) {
+					child.material = _material;
+					child.castShadow = _castShadow;
+					child.receiveShadow = _receiveShadow;
+				}
+			});
+			this.scene.add(scene);
+		}, undefined, function (e) {
+			console.error(e);
+		});
+	}
+
+	loadFloorModel() {
+		this.GLTFLoader = new THREE.GLTFLoader();
+		let scaleFactor = 2;
+
+		this.loadModel('models/itp/ceiling.glb', this.ceilingMaterial, scaleFactor, true, false);
+		this.loadModel('models/itp/floor.glb', this.floorMaterial, scaleFactor, false, true);
+		this.loadModel('models/itp/glass-fixturing.glb', this.glassFixturingMaterial, scaleFactor, true, false);
+		this.loadModel('models/itp/glass.glb', this.glassMaterial, scaleFactor, false, false);
+		this.loadModel('models/itp/granite-bar.glb', this.graniteBarMaterial, scaleFactor, true, false);
+		this.loadModel('models/itp/ibeam.glb', this.paintedMetalMaterial, scaleFactor, true, false);
+		this.loadModel('models/itp/light-diffuser.glb', this.lightDiffuserMaterial, scaleFactor, false, false);
+		this.loadModel('models/itp/light-housing.glb', this.lightHousingMaterial, scaleFactor, false, false);
+		this.loadModel('models/itp/lighting-grid.glb', this.wallMaterial, scaleFactor, false, false);
+		this.loadModel('models/itp/walls.glb', this.wallMaterial, scaleFactor, true, false);
+		this.loadModel('models/itp/window-shelf.glb', this.windowShelfMaterial, scaleFactor, true, false);
+		this.loadModel('models/itp/wooden-bar.glb', this.floorMaterial, scaleFactor, true, true);
+	}
+
+	//////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////
+	// Clients 👫
+
+	addSelf() {
 		let _body = new THREE.Mesh(
 			new THREE.BoxGeometry(1, 1, 1),
 			new THREE.MeshNormalMaterial()
@@ -61,91 +303,6 @@ class Scene {
 
 		// add group to scene
 		this.scene.add(this.playerGroup);
-
-		// Raycaster
-		this.raycaster = new THREE.Raycaster();
-
-		// array to store interactable hyperlinked meshes
-		this.hyperlinkedObjects = [];
-		// instantiate a loader
-		this.textureLoader = new THREE.TextureLoader();
-
-		//THREE Camera
-		this.camera = new THREE.PerspectiveCamera(60, this.width / this.height, 0.1, 5000);
-		this.camera.position.set(0, 3, 3);
-		this.scene.add(this.camera);
-
-		// create an AudioListener and add it to the camera
-		this.listener = new THREE.AudioListener();
-		// this.camera.add(this.listener);
-		this.playerGroup.add(this.listener);
-
-		//THREE WebGL renderer
-		this.renderer = new THREE.WebGLRenderer({
-			antialiasing: true
-		});
-
-
-		this.renderer.setClearColor(new THREE.Color(clearColor));
-
-		// add controls:
-		// https://github.com/PiusNyakoojo/PlayerControls
-		this.controls = new THREE.PlayerControls(this.camera, this.playerGroup);
-
-		this.renderer.setSize(this.width, this.height);
-
-		// add some lights
-		this.scene.add(new THREE.AmbientLight(0x404040, 4));
-
-		// White directional light at half intensity shining from the top.
-		//https://threejs.org/docs/#api/en/lights/DirectionalLight
-		var directionalLight = new THREE.DirectionalLight(0xffffff, 2);
-		this.scene.add(directionalLight);
-		var targetObject = new THREE.Object3D();
-		targetObject.position.set(10, 0, 10);
-		this.scene.add(targetObject);
-
-		directionalLight.target = targetObject;
-
-		// environment map from three.js examples
-		var path = 'models/Park2/';
-		var format = '.jpg';
-		this.envMap = new THREE.CubeTextureLoader().load([
-			path + 'posx' + format, path + 'negx' + format,
-			path + 'posy' + format, path + 'negy' + format,
-			path + 'posz' + format, path + 'negz' + format
-		]);
-		this.scene.background = this.envMap;
-
-		// load floor model
-		var loader = new THREE.GLTFLoader();
-		this.floorModel = null;
-		loader.load('models/2020-04-07--ITP-for-threejs.glb', (gltf) => {
-
-			this.floorModel = gltf.scene;
-			this.floorModel.position.set(0, 0, 0);
-			this.floorModel.scale.set(2, 2, 2); // seems about right
-			this.scene.add(this.floorModel);
-		}, undefined, function (e) {
-			console.error(e);
-		});
-
-		this.createHyperlinkedMesh(5, 0, 0, "http://www.example.com");
-		//Push the canvas to the DOM
-		domElement.append(this.renderer.domElement);
-
-		//Setup event listeners for events and handle the states
-		window.addEventListener('resize', e => this.onWindowResize(e), false);
-		domElement.addEventListener('mouseenter', e => this.onEnterCanvas(e), false);
-		domElement.addEventListener('mouseleave', e => this.onLeaveCanvas(e), false);
-		window.addEventListener('keydown', e => this.onKeyDown(e), false);
-		window.addEventListener('keyup', e => this.onKeyUp(e), false);
-
-		this.helperGrid = new THREE.GridHelper(500, 500);
-		this.helperGrid.position.y = -0.1; // offset the grid down to avoid z fighting with floor
-		this.scene.add(this.helperGrid);
-
-		this.update();
 	}
 
 	// add a client meshes, a video element and  canvas for three.js video texture
@@ -193,21 +350,6 @@ class Scene {
 		removeClientVideoElementAndCanvas(_id);
 	}
 
-	createHyperlinkedMesh(x, y, z, url) {
-		// load a image resource
-		let tex = this.textureLoader.load('images/grid.jpg');
-
-		var geometry = new THREE.CylinderGeometry(1, 1, 8, 32);
-		var material = new THREE.MeshBasicMaterial({ map: tex, color: 0xffff00 });
-		var mesh = new THREE.Mesh(geometry, material);
-		mesh.position.set(x, y, z);
-
-		// https://stackoverflow.com/questions/24690731/three-js-3d-models-as-hyperlink/24692057
-		mesh.userData = { URL: url };
-		this.hyperlinkedObjects.push(mesh);
-		this.scene.add(mesh);
-	}
-
 	// overloaded function can deal with new info or not
 	updateClientPositions(_clientProps) {
 
@@ -236,23 +378,24 @@ class Scene {
 		}
 	}
 
-	// color mode
-	crazyColors() {
-		this.floorModel.traverse((child) => {
-			if (child.isMesh) {
-				if (this.crazyMode) {
-					// if we're already in crazy mode, restore materials
-					child.material = this.originalFloorMaterials.pop();
-				} else {
-					this.originalFloorMaterials.push(child.material);
-					child.material = new THREE.MeshLambertMaterial({ color: Math.random() * 0xffffff });
-				}
-			}
-		});
-		this.crazyMode = !this.crazyMode;
+	//////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////
+	// Interaction 🤾‍♀️
+
+	createHyperlinkedMesh(x, y, z, url) {
+		// load a image resource
+		let tex = new THREE.TextureLoader().load('images/grid.jpg');
+
+		var geometry = new THREE.CylinderGeometry(1, 1, 8, 32);
+		var material = new THREE.MeshBasicMaterial({ map: tex, color: 0xffff00 });
+		var mesh = new THREE.Mesh(geometry, material);
+		mesh.position.set(x, y, z);
+
+		// https://stackoverflow.com/questions/24690731/three-js-3d-models-as-hyperlink/24692057
+		mesh.userData = { URL: url };
+		this.hyperlinkedObjects.push(mesh);
+		this.scene.add(mesh);
 	}
-
-
 
 	// TODO...
 	//https://github.com/stemkoski/stemkoski.github.com/blob/master/Three.js/Collision-Detection.html
@@ -264,41 +407,39 @@ class Scene {
 		dirVec.normalize();
 
 		this.raycaster.set(this.playerGroup.position, dirVec);
+		var intersects = this.raycaster.intersectObjects(this.hyperlinkedObjects);
 
-		if (this.floorModel != null) {
-			// console.log(this.floorModel.children);
-			// let meshArr = [];
-			// only grab the important stuff, no need to check intersection with lights
-			// meshArr = meshArr.concat(this.floorModel.children[2].children);
-			// meshArr = meshArr.concat(this.floorModel.children[7].children);
-			// meshArr = meshArr.concat(this.floorModel.children[10].children);
-
-			// var intersects = this.raycaster.intersectObjects(meshArr);
-			var intersects = this.raycaster.intersectObjects(this.hyperlinkedObjects);
-
-
-			if (intersects.length > 0) {
-				for (let i = 0; i < intersects.length; i++) {
-					if (intersects[i].distance < 1) {
-						console.log("Approaching obstacle!");
-						console.log(intersects[i].object.userData.URL);
-						if (!intersects[i].object.userData.linkVisited) {
-							// open some sort of modal asking users if they wish to enter zoom link...
-							window.open(intersects[i].object.userData.URL);
-							intersects[i].object.userData.linkVisited = true
-						}
+		if (intersects.length > 0) {
+			for (let i = 0; i < intersects.length; i++) {
+				if (intersects[i].distance < 1) {
+					console.log("Approaching obstacle!");
+					console.log(intersects[i].object.userData.URL);
+					if (!intersects[i].object.userData.linkVisited) {
+						// open some sort of modal asking users if they wish to enter zoom link...
+						window.open(intersects[i].object.userData.URL);
+						intersects[i].object.userData.linkVisited = true
 					}
 				}
 			}
 		}
+		// }
 	}
 
+	getPlayerPosition() {
+		// TODO: use quaternion or are euler angles fine here?
+		return [
+			[this.playerGroup.position.x, this.playerGroup.position.y, this.playerGroup.position.z],
+			[this.playerGroup.quaternion._x, this.playerGroup.quaternion._y, this.playerGroup.quaternion._z, this.playerGroup.quaternion._w]];
+	}
 
-	// LOOP
+	//////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////
+	// Rendering 🎥
+
 	update() {
 		requestAnimationFrame(() => this.update());
 
-		// send movement stats to the server if any of the keys are pressed
+		// send movement stats to the socket server if any of the keys are pressed
 		let sendStats = false;
 		for (let i in this.keyState) {
 			if (this.keyState[i]) {
@@ -312,8 +453,14 @@ class Scene {
 		this.updatePositions();
 		this.detectCollisions();
 		this.controls.update();
-
+		this.stats.update();
 		this.render();
+	}
+
+	render() {
+		// Update video canvases for each client
+		this.updateVideoTextures();
+		this.renderer.render(this.scene, this.camera);
 	}
 
 	updateVideoTextures() {
@@ -350,11 +497,9 @@ class Scene {
 		}
 	}
 
-	render() {
-		// Update video canvases for each client
-		this.updateVideoTextures();
-		this.renderer.render(this.scene, this.camera);
-	}
+	//////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////
+	// Event Handlers 🍽
 
 	onWindowResize(e) {
 		this.width = window.innerWidth;
@@ -384,26 +529,13 @@ class Scene {
 		event = event || window.event;
 		this.keyState[event.keyCode || event.which] = false;
 	}
-
-	getPlayerPosition() {
-		// TODO: use quaternion or are euler angles fine here?
-		return [
-			[this.playerGroup.position.x, this.playerGroup.position.y, this.playerGroup.position.z],
-			[this.playerGroup.quaternion._x, this.playerGroup.quaternion._y, this.playerGroup.quaternion._z, this.playerGroup.quaternion._w]];
-	}
 }
 
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+// Utilities 🚂
 
-
-
-
-
-
-
-
-
-
-// created <video> element for local element
+// created <video> element for local mediastream
 function createLocalVideoElement() {
 	const videoElement = document.createElement("video");
 	videoElement.id = "local_video";
