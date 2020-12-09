e5851d9523fa5f1da8d937c269b56bcaafcfc266
lydiajessup <lpj234@nyu.edu>
2020-12-09 00:21:56 -0500

    creating layout

--- src/js/yorblet.js
+++ src/js/yorblet.js
@@ -1,4 +1,6 @@
 import * as THREE from "three";
+import { create3DText, createSimpleText } from './utils';
+
 
 export class Yorblet {
     constructor(scene, projectionScreenManager) {
@@ -14,6 +16,7 @@ export class Yorblet {
         //this.addPerformanceRoom();
 
         this.addFloor();
+        this.addCenterPiece();
 
     }
 
@@ -46,13 +49,24 @@ export class Yorblet {
     ///// Shape Helper Functions /////
 
 
-    drawCircle(radius, numFaces, matColor, posX, posY, posZ, rotate){
+    drawCircle(radius, numFaces, matColor, posX, posY, posZ, angle){
+        // const circlegeometry = new THREE.CircleGeometry( radius, numFaces );
+        // const circlematerial = new THREE.MeshBasicMaterial( { color: matColor } );
+        // const circle = new THREE.Mesh( circlegeometry, circlematerial );
+        // circle.position.set(posX, posY, posZ);
+        // circle.rotateY(rotate);
+        // this.scene.add( circle );
+
+        
         const circlegeometry = new THREE.CircleGeometry( radius, numFaces );
         const circlematerial = new THREE.MeshBasicMaterial( { color: matColor } );
         const circle = new THREE.Mesh( circlegeometry, circlematerial );
         circle.position.set(posX, posY, posZ);
-        circle.rotateY(rotate);
+        circle.rotateY(angle);
+        circle.lookAt(0,0,0);
         this.scene.add( circle );
+
+
 
       }
 
@@ -128,6 +142,8 @@ export class Yorblet {
             let theta = ((Math.PI * 2) / numProjects);
             let angle = (theta * i);
 
+            console.log("angle: ", angle);
+
             let centerX = radius * Math.cos(angle);
             let centerZ = radius * Math.sin(angle);
             this.addPresentationStage(centerX, centerZ, 1, angle);
@@ -154,18 +170,64 @@ export class Yorblet {
         var colPurple = 0x9588CB;
         var colBlue = 0x05C1DA;
 
-
-
         //test circle
-
-        const circlegeometry = new THREE.CircleGeometry( 5, 32 );
-        const circlematerial = new THREE.MeshBasicMaterial( { color: 0x000000 } );
-        const circle = new THREE.Mesh( circlegeometry, circlematerial );
-        circle.position.set(centerX, 0, centerZ);
-        circle.rotateY(angle);
-        circle.lookAt(0,0,0);
-        this.scene.add( circle );
+        // const circlegeometry = new THREE.CircleGeometry( 5, 32 );
+        // const circlematerial = new THREE.MeshBasicMaterial( { color: 0x000000 } );
+        // const circle = new THREE.Mesh( circlegeometry, circlematerial );
+        // circle.position.set(centerX, 0, centerZ);
+        // circle.rotateY(angle);
+        // circle.lookAt(0,0,0);
+        // this.scene.add( circle );
         //this works!
+
+
+        this.drawCircle(4, 32, colPurple, centerX, 4, centerZ, angle);
+
+        let xshift_white = 30 * Math.cos(angle+0.15);
+        let zshift_white = 30 * Math.sin(angle+0.15);
+        this.drawCircle(3, 32, colWhite, (xshift_white), 7, (zshift_white), angle);
+        //console.log("circle: ", centerX);
+
+        //med circles
+        let xshift_green = 30 * Math.cos(angle-0.14);
+        let zshift_green = 30 * Math.sin(angle-0.14);
+
+        this.drawCircle(1, 32, colGreen, (xshift_green), 4, (zshift_green), angle);
+
+        let xshift_black1 = 30 * Math.cos(angle-0.20);
+        let zshift_black1 = 30 * Math.sin(angle-0.20);
+        this.drawCircle(2, 32, colBlack, xshift_black1, 7, zshift_black1, angle);
+        //
+        // //small circles
+        let xshift_black2 = 30 * Math.cos(angle+0.20);
+        let zshift_black2 = 30 * Math.sin(angle+0.20);
+        this.drawCircle(1, 32, colPurple, xshift_black2, 2, zshift_black2, angle);
+
+        let xshift_pink = 30 * Math.cos(angle-0.12);
+        let zshift_pink = 30 * Math.sin(angle-0.12);
+        this.drawCircle(0.5, 32, colPink, (xshift_pink), 5, (zshift_pink), angle);
+
+
+        //draw fence
+        let xshift_black3 = 30 * Math.cos(angle+0.3);
+        let zshift_black3 = 30 * Math.sin(angle+0.3);
+        this.drawCircle(1, 32, colPurple, xshift_black3, 2.5, zshift_black3, angle);
+
+        //draw fence
+        let xshift_black4 = 30 * Math.cos(angle+0.4);
+        let zshift_black4 = 30 * Math.sin(angle+0.4);
+        this.drawCircle(1, 32, colPurple, xshift_black4, 2, zshift_black4, angle);
+
+        //draw fence
+        let xshift_black5 = 30 * Math.cos(angle+0.5);
+        let zshift_black5 = 30 * Math.sin(angle+0.5);
+        this.drawCircle(1, 32, colPurple, xshift_black5, 2.5, zshift_black5, angle);
+
+        //draw fence
+        let xshift_black6 = 30 * Math.cos(angle+0.6);
+        let zshift_black6 = 30 * Math.sin(angle+0.6);
+        this.drawCircle(1, 32, colPurple, xshift_black6, 2, zshift_black6, angle);
+
 
 
         // this.drawCircle(8, 32, colPurple, (centerX-2), (12), (centerZ-15), angle);
@@ -179,7 +241,116 @@ export class Yorblet {
         // this.drawCircle(2, 32, colBlack, (centerX-10), 18, (centerZ-15), angle);
         // this.drawCircle(1, 32, colPink, (centerX-3), 12, (centerZ-15), angle);
 
+
+        // //add label
+        // let textDepth = 0.1;
+        // let curveSegments = 3;
+        // let message, txt;
+        //
+        // message = 'Lydia Jessup!';
+        //
+        // // const loader = new THREE.FontLoader();
+        // // const font = loader.load('../assets/fonts/helvetiker_regular_copy.typeface.json');
+        //
+        // const fontJson = require('../assets/fonts/helvetiker_regular_copy.typeface.json' );
+        // const font = new THREE.Font( fontJson );
+        //
+        // // params: text, size, depth, curveSegments, bevelThickness, bevelSize, bevelEnabled, mirror, fontMesh
+        // txt = create3DText(message, 0.25, textDepth, curveSegments, 0.01, 0.01, false, false, font);
+        //
+        // //draw fence
+        // let font_xshift = 30 * Math.cos(angle+0.2);
+        // let font_zshift = 30 * Math.sin(angle+0.2);
+        //
+        // txt.position.set(0, 0, 0);
+        // //txt.rotateY(angle);
+        // //txt.lookAt(0,0,0);
+        // this.scene.add(txt);
+
+
+//this isn't workinng
+            //
+            // const loader = new THREE.FontLoader();
+            //
+            // loader.load('../assets/fonts/helvetiker_regular_copy.typeface.json', (font) => {
+            // //
+
+
+            const fontJson = require('../assets/fonts/helvetiker_regular_copy.typeface.json' );
+            const font = new THREE.Font( fontJson );
+
+              const text = 'Lydia Jessup';
+
+              const fontGeometry = new THREE.TextBufferGeometry(text, {
+                font: font,
+                size:  .25,
+                height:  .2,
+                curveSegments: 11,
+                bevelEnabled: false,
+                bevelThickness: 0.05,
+                bevelSize: 0.10,
+                bevelSegments: 6,
+              });
+
+              const fontMaterial = new THREE.MeshPhongMaterial( {color: 0xc6fc03, flatShading: true} );
+              const fontMesh = new THREE.Mesh(fontGeometry, fontMaterial);
+
+              let font_xshift = 30 * Math.cos(angle+0.1);
+              let font_zshift = 30 * Math.sin(angle+0.1);
+              fontMesh.position.set(font_xshift, 1.5, font_zshift);
+              fontMesh.rotateY(angle);
+              fontMesh.lookAt(0,0,0);
+              this.scene.add( fontMesh );
+
+          //  });
+
+
+
         this.projectionScreenManager.addScreen(centerX,2,centerZ,0,2,0, scaleFactor);
+    }
+
+
+    addCenterPiece(){
+
+      // add table
+
+      // const tableGeometry = new THREE.CylinderBufferGeometry(1, 1, 2, 32, 1, false);
+      // const tableMaterial = new THREE.MeshPhongMaterial({ color: 0xFB69B9, side: THREE.DoubleSide });
+      // const table = new THREE.Mesh(tableGeometry, tableMaterial);
+      // table.position.set(0, 0, 0);
+      // //cylinder.lookAt(0,0,0);
+      // this.scene.add(table);
+
+      const centerGeometry = new THREE.SphereGeometry( 1, 32, 32 );
+      const centerMaterial = new THREE.MeshPhongMaterial( {color: 0xFB69B9} );
+      const center = new THREE.Mesh( centerGeometry, centerMaterial );
+      center.position.set(0, 0, 0);
+      this.scene.add( center );
+
+
+      const fontJson = require('../assets/fonts/helvetiker_regular_copy.typeface.json' );
+      const font = new THREE.Font( fontJson );
+
+        const text = 'Circle Room';
+
+        const fontGeometry = new THREE.TextBufferGeometry(text, {
+          font: font,
+          size:  .25,
+          height:  .01,
+          curveSegments: 11,
+          bevelEnabled: true,
+          bevelThickness: 0.01,
+          bevelSize: 0.01,
+          bevelSegments: 1,
+        });
+
+        const fontMaterial = new THREE.MeshPhongMaterial( {color: 0xc6fc03, flatShading: true} );
+        const fontMesh = new THREE.Mesh(fontGeometry, fontMaterial);
+        fontMesh.position.set(-1, 2, 0);
+
+        this.scene.add( fontMesh );
+
+
     }
 
     addPerformanceRoom(){
