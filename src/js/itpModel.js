61645d50ebb11c04b8773d0fb57a95a2b4826203
AidanNelson <AidanNelson@users.noreply.github.com>
2021-04-05 20:07:05 -0400

    update three version

--- src/js/itpModel.js
+++ src/js/itpModel.js
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 export class ITPModel {
     constructor(scene) {
